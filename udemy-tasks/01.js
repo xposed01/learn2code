b1fa00b442c26f3e524d805c819bfe450d9541b1
xposed01 <43813249+xposed01@users.noreply.github.com>
2022-08-11 16:22:06 +0700

    feat: add validation

--- udemy-tasks/01.js
+++ udemy-tasks/01.js
@@ -1,18 +1,50 @@
 'use strict';
 
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
-let lastWatchedFilm = prompt('Ваш последний просмотренный фильм?');
-let lastWatchedFilmScore = prompt('Поставьте оценку этому фильму');
+
 
 let personalMovieDB = {
-  count: 1,
+  count: numberOfFilms,
   movies: {},
   actors: {},
   genres: [],
   privat: false,
 };
 
-personalMovieDB.movies[lastWatchedFilm] = lastWatchedFilmScore;
+if (personalMovieDB.count < 10) {
+  alert('Просмотрено довольно мало фильмов');
+}
+if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  alert('Вы классический зритель');
+}
+if (personalMovieDB.count > 30) {
+  alert('Вы киноман');
+}
+
+let i = 0;
+while (i < 2) {
+  let lastWatchedFilm = prompt('Ваш последний просмотренный фильм?');
+
+  if (lastWatchedFilm == ' ') {
+    alert('Введите название фильма');
+  }
+
+  if (lastWatchedFilm.length > 50) {
+      alert('Название фильма должно быть короче 50 символов.');
+    } else {
+
+    let lastWatchedFilmScore = prompt('Поставьте оценку этому фильму');
+    if (lastWatchedFilm && lastWatchedFilmScore) {
+      personalMovieDB.movies[lastWatchedFilm] = lastWatchedFilmScore;
+      i++;
+
+    }
+  }
+}
+
+
+
+
+
 
 console.log(personalMovieDB);
-
