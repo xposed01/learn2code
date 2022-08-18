46c54318fb17ca27cfb1babb2ec50dc6ea016bae
xposed01 <43813249+xposed01@users.noreply.github.com>
2022-08-18 21:24:05 +0700

    feat: add sort func

--- udemy-tasks/01 - movie db/js/script.js
+++ udemy-tasks/01 - movie db/js/script.js
@@ -1,17 +1,3 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
 const movieDB = {
@@ -23,4 +9,29 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
+
+// 1) Удалить все рекламные блоки со страницы
+
+document.querySelectorAll('.promo__adv img').forEach(element => {
+    element.remove();
+});
+
+// 2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+document.querySelector('.promo__genre').textContent = 'ДРАМА';
+
+// 3) Изменить задний фон постера 
+
+document.querySelector('.promo__bg').style.background = 'url("../img/bg.jpg") center center/cover no-repeat';
+
+// 4) Список фильмов на странице сформировать на основании данных JS файла.
+// Отсортировать по алфавиту. Добавить нумерацию выведенных фильмов
+
+movieDB.movies.sort();
+
+document.querySelectorAll('.promo__interactive-item').forEach((element, index) => {
+    element.innerHTML = `
+    ${index+1}) ${movieDB.movies[index]}
+    <div class="delete"></div>`;
+});
 
